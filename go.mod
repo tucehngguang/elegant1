4d8a9ca0881f4e06d7061652af83f23b0b8c551d
tcgvoyager <1369115031@qq.com>
2024-05-27 13:12:45 +0800

    mysqlfirstready

--- go.mod
+++ go.mod
@@ -2,8 +2,14 @@ module main
 
 go 1.22.3
 
+require github.com/go-redis/redis/v8 v8.11.5
+
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/go-redis/redis/v8 v8.11.5 
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	gorm.io/driver/mysql v1.5.6 // direct
+	gorm.io/gorm v1.25.10 // direct
 )
--- mian.go
+++ mian.go
@@ -7,60 +7,125 @@ import (
 	"time"
 
 	"github.com/go-redis/redis/v8"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-type APN struct {
-	name       string
-	useage     int  //流量使用量
-	Tlimit     int  //流量上限
-	Expiration Time //到期时间
-}
 type Time struct {
 	year  int
 	month time.Month
 	day   int
 } //到期时间
 type Sim struct {
-	iccid      string
-	imsi       string
-	msisdn     string //三码
-	state      string
-	useage     int  //流量使用量
-	Tlimit     int  //流量上限
-	Expiration Time //到期时间12121212666454556454
-	apn        [1000]APN
-} //已修改
+	gorm.Model
+	ICCID      string    `gorm:"type:varchar(12)"`
+	IMSI       string    `gorm:"type:varchar(12)"`
+	MSISDN     string    `gorm:"type:varchar(12)"`
+	STATE      string    `gorm:"type:varchar(12)"`
+	USEAGE     int       //流量使用量
+	Tlimit     int       //流量上限
+	Expiration time.Time //到期时间12121212666454556454
+	APN        []APN     `gorm:"many2many: sim_apn;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
 
+type APN struct {
+	gorm.Model
+	ICCID      string    `gorm:"type:varchar(12)"`
+	NAME       string    `gorm:"type:varchar(12)"`
+	USEAGE     int       //流量使用量
+	Tlimit     int       //流量上限
+	Expiration time.Time //到期时间
+	Sims       []Sim     `gorm:"many2many: sim_apn;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
+
+// 已修改
+func creatsql(dsn string) {
+	var i int
+	db, err := gorm.Open(mysql.Open(dsn))
+	if err != nil {
+		panic("连接数据库失败，error=" + err.Error())
+	}
+
+	for i = 1; i < 5; i++ {
+
+		// 将字符串转换为整数
+
+		// 将整数加1
+
+		// 将结果转换为字符串
+		result := strconv.Itoa(i)
+		sim3 := Sim{
+			ICCID:      result,
+			IMSI:       "31",
+			MSISDN:     "tcg",
+			STATE:      "ac",
+			USEAGE:     150,
+			Tlimit:     1000,
+			Expiration: time.Now().AddDate(1, 0, 0), // 假设一年后到期
+			APN: []APN{
+				{
+					ICCID:      "210",
+					NAME:       "apn1",
+					USEAGE:     100,
+					Tlimit:     500,
+					Expiration: time.Now().AddDate(1, 0, 0),
+				},
+				{
+					ICCID:      "210",
+					NAME:       "apn2",
+					USEAGE:     100,
+					Tlimit:     500,
+					Expiration: time.Now().AddDate(1, 0, 0),
+				},
+			},
+		}
+		var err error
+		err = db.Create(&sim3).Error
+		fmt.Println(err) //创建
+	}
+}
 func menu() {
 
-	fmt.Println("--------系统介绍-------------")
-	fmt.Println("sim卡管理系统")
-	fmt.Println("1为状态激活")
-	fmt.Println("2是变更卡的流量上限")
-	fmt.Println("3是查看卡的使用流量是否达到上限")
-	fmt.Println("4是输入卡的到期时间")
+	fmt.Printf("--------系统介绍-------------\n")
+	fmt.Printf("sim卡管理系统\n")
+	fmt.Printf("1为状态激活\n")
+	fmt.Printf("2是变更卡的流量上限\n")
+	fmt.Printf("3是查看卡的使用流量是否达到上限\n")
+	fmt.Printf("4是输入卡的到期时间\n")
 	fmt.Println("5为查看卡是否到期")
+	fmt.Println("6为打印sim卡信息")
 	fmt.Println("-----------------------------")
 }
 func activation(S *Sim) {
-	if S.state == "未启用" {
-		S.state = "激活"
-		fmt.Printf("%s", S.state)
+
+	if S.STATE == "未启用" {
+		S.STATE = "激活"
+		fmt.Printf("%s", S.STATE)
 		return
 	}
-	if S.state == "停用" {
+	if S.STATE == "停用" {
 		fmt.Printf("sim卡已停用无法激活\n")
 		return
 	}
-	if S.state == "激活" {
+	if S.STATE == "激活" {
 		fmt.Printf("sim卡已激活无法再次激活\n")
 		return
 	}
 } //激活
 func change(S *Sim, a1 int, a2 int, client *redis.Client) {
 	hashKey := "SIM1"
-	S.apn[1].Tlimit = a1
-	S.apn[2].Tlimit = a2
+	a := []int{a1, a2}
+
+	for i := range S.APN {
+		S.APN[i].Tlimit = a[i]
+
+	}
+	// maxDataLimit := 0
+	// // for _, apn := range S.APN {
+	// // 	if apn.Tlimit > maxDataLimit {
+	// // 		maxDataLimit = apn.Tlimit
+	// // 	}
+	// // }
 	S.Tlimit = max(a1, a2)
 	judge, erro := client.HExists(context.Background(), hashKey, "Tlimit").Result()
 	if erro != nil {
@@ -89,19 +154,18 @@ func change(S *Sim, a1 int, a2 int, client *redis.Client) {
 	} else {
 		fmt.Println("not")
 	} //判断是否存在改卡的流量上限
-
 	fmt.Printf("apn1的流量上限为%dKB\n", a1)
 	fmt.Printf("apn2的流量上限为%dKB\n", a2)
 	fmt.Printf("sim卡的流量上限为%dKB\n", S.Tlimit)
 } //查看流量上限
 func detection(S *Sim, ua1 int, ua2 int) {
 	fmt.Printf("请输入使用的流量\n")
-	S.useage = ua1 + ua2
-	S.apn[1].useage = ua1
-	S.apn[2].useage = ua2
-	if S.apn[1].useage > S.apn[1].Tlimit || S.apn[2].useage > S.apn[2].Tlimit || S.useage > S.Tlimit {
-		S.state = "停用"
-		fmt.Printf("卡已到期sim卡的状态为%s\n", S.state)
+	S.USEAGE = ua1 + ua2
+	S.APN[1].USEAGE = ua1
+	S.APN[2].USEAGE = ua2
+	if S.APN[1].USEAGE > S.APN[1].Tlimit || S.APN[2].USEAGE > S.APN[2].Tlimit || S.USEAGE > S.Tlimit {
+		S.STATE = "停用"
+		fmt.Printf("卡已到期sim卡的状态为%s\n", S.STATE)
 	} else {
 		fmt.Printf("还有空余流量\n")
 
@@ -111,15 +175,16 @@ func modifydate(S *Sim, ta1 Time, ta2 Time) {
 	fmt.Printf("请输入卡的到期时间\n")
 	fmt.Scan(&ta1.year, &ta1.month, &ta1.day)
 	fmt.Scan(&ta2.year, &ta2.month, &ta2.day)
-	S.apn[1].Expiration = ta1
-	S.apn[2].Expiration = ta2
+
 	time1 := time.Date(ta1.year, ta1.month, ta1.day, 15, 10, 11, 999000111, time.Local)
 	time2 := time.Date(ta2.year, ta2.month, ta2.day, 15, 10, 11, 999000111, time.Local)
+	S.APN[0].Expiration = time1
+	S.APN[1].Expiration = time2
 	if time1.After(time2) {
-		S.Expiration = ta1
+		S.Expiration = time1
 		fmt.Printf("卡的到期时间:%d-%d-%d %d:%d:%d %s\n", time1.Year(), time1.Month(), time1.Day(), time1.Hour(), time1.Minute(), time1.Second(), time1.Weekday().String())
 	} else {
-		S.Expiration = ta2
+		S.Expiration = time2
 		fmt.Printf("卡的到期时间:%d-%d-%d %d:%d:%d %s\n", time2.Year(), time2.Month(), time2.Day(), time2.Hour(), time2.Minute(), time2.Second(), time2.Weekday().String())
 	}
 } //修改卡到期时间
@@ -127,52 +192,113 @@ func checkepire(S *Sim, ta1 Time) {
 	fmt.Printf("请输入修改的卡到期时间\n")
 	fmt.Scan(&ta1.year, &ta1.month, &ta1.day)
 	time1 := time.Date(ta1.year, ta1.month, ta1.day, 15, 10, 11, 999000111, time.Local)
-	time2 := time.Date(S.Expiration.year, S.Expiration.month, S.Expiration.day, 15, 10, 11, 999000111, time.Local)
-	if time1.After(time2) {
-		S.state = "停用"
-		fmt.Printf("卡已到期sim卡的状态为%s\n", S.state)
+
+	if time1.After(S.Expiration) {
+		S.STATE = "停用"
+		fmt.Printf("卡已到期sim卡的状态为%s\n", S.STATE)
 	} else {
 		fmt.Printf("sim卡未到期还能继续使用\n")
 	}
 }
 func printsim(S *Sim) {
-	fmt.Printf("Sim iccid为: %v\n", S.iccid)
-	fmt.Printf("Sim imsi为: %v\n", S.imsi)
-	fmt.Printf("Sim msisdn为: %v\n", S.msisdn)
-	fmt.Printf("Sim 状态为: %v\n", S.state)
-	fmt.Printf("Sim 流量使用量为: %v\n", S.useage)
+	fmt.Printf("Sim iccid为: %v\n", S.ICCID)
+	fmt.Printf("Sim imsi为: %v\n", S.IMSI)
+	fmt.Printf("Sim msisdn为: %v\n", S.MSISDN)
+	fmt.Printf("Sim 状态为: %v\n", S.STATE)
+	fmt.Printf("Sim 流量使用量为: %v\n", S.USEAGE)
 	fmt.Printf("Sim 流量上限为: %v\n", S.Tlimit)
 	fmt.Printf("Sim 到期时间为: %#v\n", S.Expiration)
 }
+
+// 定义一个全局变量db，用于后面数据库的读写操作,通常就放在全局里面
+var DB *gorm.DB
+
 func main() {
 	var choice int
 	var la1 int
 	var la2 int //流量上限
 	var ua1 int
 	var ua2 int //使用流量
-	var sim Sim
+
 	var ta1 Time
-	var ta2 Time //到期时间
-	sim.iccid = "xx"
-	sim.imsi = "xxx"
-	sim.msisdn = "xxxx"
-	sim.state = "激活" //卡初始状态
-	sim.apn[1].name = "apn1"
-	sim.apn[2].name = "apn2" //apn名字
-	sim.apn[1].Tlimit = 6
-	sim.apn[2].Tlimit = 4
-	sim.Tlimit = 6 //截止时间
-	sim.Expiration.year = 2024
-	sim.Expiration.month = 6
-	sim.Expiration.day = 30
-	sim.apn[1].Expiration.year = 2024
-	sim.apn[1].Expiration.month = 6
-	sim.apn[1].Expiration.day = 30
-	sim.apn[2].Expiration.year = 2024
-	sim.apn[2].Expiration.month = 5
-	sim.apn[2].Expiration.day = 1
-	//卡的截止时间
-	menu() //菜单打印
+	var ta2 Time        //到期时间
+	username := "root"  //账号
+	password := "123"   //密码
+	host := "localhost" //数据库地址
+	port := "3306"      //端口
+	Dnname := "study"   //数据库名
+	timeout := "10s"    //连接超时，10s
+	//root:root@tcp(127.0.0.1:3306)/test？
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?timeout=%s", username, password, host, port, Dnname, timeout)
+	//连接mysql，获得DB类型实例，用于后面数据库的读写操作
+	db, err := gorm.Open(mysql.Open(dsn))
+	if err != nil {
+		panic("连接数据库失败，error=" + err.Error())
+	}
+	// 创建Sim4实例并关联APN
+
+	sim := []Sim{
+		{ICCID: "210",
+			IMSI:       "31",
+			MSISDN:     "123",
+			STATE:      "ac",
+			USEAGE:     150,
+			Tlimit:     1000,
+			Expiration: time.Now().AddDate(1, 0, 0), // 假设一年后到期
+			APN: []APN{
+				{
+					ICCID:      "210",
+					NAME:       "apn1",
+					USEAGE:     100,
+					Tlimit:     500,
+					Expiration: time.Now().AddDate(1, 0, 0),
+				},
+				{
+					ICCID:      "210",
+					NAME:       "apn2",
+					USEAGE:     100,
+					Tlimit:     500,
+					Expiration: time.Now().AddDate(1, 0, 0),
+				},
+			},
+		},
+		{ICCID: "21",
+			IMSI:       "31",
+			MSISDN:     "123",
+			STATE:      "ac",
+			USEAGE:     150,
+			Tlimit:     1000,
+			Expiration: time.Now().AddDate(1, 0, 0), // 假设一年后到期
+			APN: []APN{
+				{
+					ICCID:      "210",
+					NAME:       "apn1",
+					USEAGE:     100,
+					Tlimit:     500,
+					Expiration: time.Now().AddDate(1, 0, 0),
+				},
+				{
+					ICCID:      "210",
+					NAME:       "apn2",
+					USEAGE:     100,
+					Tlimit:     500,
+					Expiration: time.Now().AddDate(1, 0, 0),
+				},
+			},
+		},
+	}
+
+	// 输出结果以验证
+	fmt.Printf("Sim4 ICCID: %s, APNs: %d\n", sim[0].ICCID, len(sim[0].APN))
+	for _, apn := range sim[1].APN {
+		fmt.Printf("APN NAME: %s, USEAGE: %d\n", apn.NAME, apn.USEAGE)
+	}
+
+	DB = db
+	DB.AutoMigrate(&Sim{})
+	err = db.Create(&sim[0]).Error
+	fmt.Println(err) //创建
+	menu()           //菜单打印
 	client := redis.NewClient(&redis.Options{
 		Addr:     "192.168.24.225:6379", // Redis服务器地址
 		Password: "",                    // 可选：如果有密码的话
@@ -184,30 +310,27 @@ func main() {
 
 	// 使用HMSet方法设置多个哈希字段及其对应的值
 	fieldsAndValues := map[string]interface{}{
-		"ICCID":            "xx",
-		"IMSI":             "xxx",
-		"MSISDN":           "xxxx",
-		"State":            "未启用",
-		"Tlimit":           "6",
-		"Expirationyear":   "2024",
-		"Expirationymonth": "6",
-		"Expirationday":    "30",
+		"ICCID":      "xx",
+		"IMSI":       "xxx",
+		"MSISDN":     "xxxx",
+		"State":      "未启用",
+		"Tlimit":     "6",
+		"Expiration": "2024-3-2",
 	}
 
-	err := client.HMSet(context.Background(), hashKey, fieldsAndValues).Err()
-	if err != nil {
+	erro := client.HMSet(context.Background(), hashKey, fieldsAndValues).Err()
+	if erro != nil {
 		fmt.Println("Error setting hash fields:", err)
 		return
 	}
 
 	for {
 		fmt.Println("是否开始操作")
-		fmt.Println("1为状态激活")
-		fmt.Println("2是变更卡的流量上限")
-		fmt.Println("3是查看卡的使用流量是否达到上限")
-		fmt.Println("4是输入卡的到期时间")
-		fmt.Println("5为查看卡是否到期")
-		fmt.Println("-----------------------")
+		fmt.Printf("1为状态激活\n")
+		fmt.Printf("2是变更卡的流量上限\n")
+		fmt.Printf("3是查看卡的使用流量是否达到上限\n")
+		fmt.Printf("4是输入卡的到期时间\n")
+		fmt.Printf("5为查看卡是否到期\n")
 		fmt.Scan(&choice)
 		switch choice {
 		case 0:
@@ -218,7 +341,7 @@ func main() {
 			}
 		case 1:
 			{
-				activation(&sim)
+				activation(&sim[0])
 
 			} //激活卡
 
@@ -226,29 +349,34 @@ func main() {
 			{
 				fmt.Printf("请输入流量上限\n")
 				fmt.Scan(&la1, &la2)
-				change(&sim, la1, la2, client)
+				change(&sim[0], la1, la2, client)
 
 			} //变更卡的流量上限
 		case 3:
 			{
 				fmt.Printf("请输入已使用流量\n")
 				fmt.Scan(&ua1, &ua2)
-				detection(&sim, ua1, ua2)
+				detection(&sim[0], ua1, ua2)
 
 			} //查看卡的使用流量是否达到上限
 		case 4:
 			{
-				modifydate(&sim, ta1, ta2)
+				modifydate(&sim[0], ta1, ta2)
 
 			}
 		case 5:
 			{
-				checkepire(&sim, ta1)
+				checkepire(&sim[0], ta1)
 
 			}
 		case 6:
 			{
-				printsim(&sim)
+				printsim(&sim[0])
+			}
+		case 7:
+			{
+
+				creatsql(dsn)
 			}
 		}
 
